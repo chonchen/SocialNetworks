5f2207b030cdb387acc40a701d1b8043ca6f8f12
chonchen <chonglei.chen@gmail.com>
2016-06-24 10:32:32 -0700

    change addEdges method to include WeightedDirectedEdge

--- src/graph/WeightedGraph.java
+++ src/graph/WeightedGraph.java
@@ -4,35 +4,36 @@ import java.util.HashMap;
 
 public class WeightedGraph {
 	
-	private HashMap<Integer, HashMap<Integer, Integer>> vertices = new HashMap<Integer, HashMap<Integer, Integer>>();
+	private HashMap<Integer, HashMap<Integer, WeightedDirectedEdge>> vertices = new HashMap<Integer, HashMap<Integer, WeightedDirectedEdge>>();
 
 	public void addVertex(int num) {
 		// TODO Auto-generated method stub
 		if (!vertices.containsKey(num))
 		{
-			vertices.put(num, new HashMap<Integer, Integer>());
+			vertices.put(num, new HashMap<Integer, WeightedDirectedEdge>());
 		}
 	}
 
-	public void addEdge(int from, int to) {
+	public void addEdge(int from, int to, Edge e) {
 		// TODO Auto-generated method stub
 		if (vertices.containsKey(from) && vertices.containsKey(to))
 		{
-			HashMap<Integer, Integer> edges = vertices.get(from);
-			if (edges.containsKey(to))
+			HashMap<Integer, WeightedDirectedEdge> edgeTos = vertices.get(from);
+			if (edgeTos.containsKey(to))
 			{
-				Integer i = edges.get(to);
-				i++;
-				edges.put(to, i);
+				WeightedDirectedEdge wDEdge = edgeTos.get(to);
+				wDEdge.addEdge(e);
 			}
 			else
 			{
-				edges.put(to, 1);
+				WeightedDirectedEdge wDEdge = new WeightedDirectedEdge(from, to);
+				wDEdge.addEdge(e);
+				edgeTos.put(to, wDEdge);
 			}
 		}
 	}
 	
-	public HashMap<Integer, HashMap<Integer, Integer>> exportGraph()
+	public HashMap<Integer, HashMap<Integer, WeightedDirectedEdge>> exportGraph()
 	{
 		return vertices;
 	}
@@ -43,10 +44,10 @@ public class WeightedGraph {
 		sb.append("From     To     Weight\n");
 		for (Integer from: vertices.keySet())
 		{
-			HashMap<Integer, Integer> toVertices = vertices.get(from);
+			HashMap<Integer, WeightedDirectedEdge> toVertices = vertices.get(from);
 			for (Integer to: toVertices.keySet())
 			{
-				sb.append(from.toString() + "         " + to.toString() + "       " + toVertices.get(to).toString() +"\n");
+				sb.append(from.toString() + "         " + to.toString() + "       " + toVertices.get(to).weight() +"\n");
 			}
 		}
 		
@@ -60,9 +61,9 @@ public class WeightedGraph {
 		
 		wGraph.addVertex(0);
 		wGraph.addVertex(1);
-		wGraph.addEdge(0, 1);
-		wGraph.addEdge(0, 1);
-		wGraph.addEdge(1, 0);
+		wGraph.addEdge(0, 1, new Edge(4, 5));
+		wGraph.addEdge(0, 1, new Edge(4, 5));
+		wGraph.addEdge(1, 0, new Edge(5, 4));
 		System.out.println(wGraph.toString());
 		
 	}
