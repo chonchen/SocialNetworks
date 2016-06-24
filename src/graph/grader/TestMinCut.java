dc91b886274db65c8bd19f26d736ed20f5fa3ffa
chonchen <chonglei.chen@gmail.com>
2016-06-24 10:59:09 -0700

    Added WeightedEdge support in CommunityNetwork and TestMinCut

--- src/graph/grader/TestMinCut.java
+++ src/graph/grader/TestMinCut.java
@@ -11,6 +11,7 @@ import graph.FlowEdge;
 import graph.FlowNetwork;
 import graph.FordFulkerson;
 import graph.Graph;
+import graph.WeightedDirectedEdge;
 import graph.WeightedGraph;
 import util.GraphLoader;
 
@@ -25,16 +26,16 @@ public class TestMinCut {
 		CommunityNetwork communityNetwork = new CommunityNetwork(graph.reverse());
 		WeightedGraph weightedGraph = communityNetwork.getCommunityGraph();
 		
-		HashMap<Integer, HashMap<Integer, Integer>> wgExport = weightedGraph.exportGraph();
+		HashMap<Integer, HashMap<Integer, WeightedDirectedEdge>> wgExport = weightedGraph.exportGraph();
 		
 		FlowNetwork flowNetwork = new FlowNetwork(wgExport.size());
 		
 		for (Integer from : wgExport.keySet())
 		{
-			HashMap<Integer, Integer> edges = wgExport.get(from);
+			HashMap<Integer, WeightedDirectedEdge> edges = wgExport.get(from);
 			for(Integer to : edges.keySet())
 			{
-				int weight = edges.get(to);
+				int weight = edges.get(to).weight();
 				flowNetwork.addEdge(new FlowEdge(from, to, weight));
 			}
 		}
@@ -53,24 +54,24 @@ public class TestMinCut {
             }
         }
         
-        List<FlowEdge> communityEdges = new LinkedList<FlowEdge>();
+        List<WeightedDirectedEdge> communityEdges = new LinkedList<WeightedDirectedEdge>();
         
         for (Integer v : minCut)
         {
-        	HashMap<Integer, Integer> edge = wgExport.get(v);
+        	HashMap<Integer, WeightedDirectedEdge> edge = wgExport.get(v);
         	for (Integer e : edge.keySet())
         	{
         		if (!minCut.contains(e))
         		{
-        			Integer weight = edge.get(e);
-        			communityEdges.add(new FlowEdge(v, e, weight));
+        			WeightedDirectedEdge wDEdge = edge.get(e);
+        			communityEdges.add(wDEdge);
         		}
         	}
         }
         
-        for (FlowEdge edge : communityEdges)
+        for (WeightedDirectedEdge edge : communityEdges)
         {
-        	System.out.println("From: " + edge.from() + " To: " + edge.to() + " weight: " + edge.capacity());
+        	System.out.println("From: " + edge.fromVertex() + " To: " + edge.toVertex() + " weight: " + edge.weight());
         }
         
         
